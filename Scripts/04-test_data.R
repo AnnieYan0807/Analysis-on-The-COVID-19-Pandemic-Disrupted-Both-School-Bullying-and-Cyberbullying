5fedd790079eaa4e4a84c85f21c2bdf08e0b8411
AnnieYan0807 <yanzixin0807@gmail.com>
2023-02-20 22:46:59 -0500

    update paper

--- Scripts/04-test_data.R
+++ Scripts/04-test_data.R
@@ -12,8 +12,60 @@
 library(tidyverse)
 
 
+sum_bully_data <- read.csv(here::here("Outputs/data/sum_bully_data.csv"))
+sum_cybully_data <- read.csv(here::here("Outputs/data/sum_cybully_data.csv"))
+sum_schbully_data <- read.csv(here::here("Outputs/data/sum_schbully_data.csv"))
+sum_national_schbully_data <- read.csv(here::here("Outputs/data/sum_national_schbully_data.csv"))
+
+cleaned_bully_data <- read.csv(here::here("Outputs/data/cleaned_schbully_data.csv"))
+cleaned_cybully_data <- read.csv(here::here("Outputs/data/cleaned_schbully_data.csv"))
+cleaned_schbully_data <- read.csv(here::here("Outputs/data/cleaned_schbully_data.csv"))
+cleaned_national_schbully_data <- read.csv(here::here("Outputs/data/cleaned_national_schbully_data.csv"))
+
+
 #### Test data ####
 
-cleaned_data$date  |> class() == "Date"
-cleaned_data$ cy_bly |> min(na.rm=TRUE) >= 0
-cleaned_data$ sch_cy_bly|> max(na.rm=TRUE) <= 240 
+#cleaned_bully_data
+cleaned_bully_data$ hits  |> class() == "integer"
+cleaned_bully_data$ month |> min(na.rm=TRUE) >= 0
+cleaned_bully_data$ day|> max(na.rm=TRUE) <= 31
+
+#cleaned_cybully_data
+cleaned_cybully_data$ hits  |> class() == "integer"
+cleaned_cybully_data$ month |> min(na.rm=TRUE) >= 0
+cleaned_cybully_data$ day|> max(na.rm=TRUE) <= 31
+
+
+#cleaned_schbully_data
+cleaned_schbully_data$ hits  |> class() == "integer"
+cleaned_schbully_data$ month |> min(na.rm=TRUE) >= 0
+cleaned_schbully_data$ day|> max(na.rm=TRUE) <= 31
+
+#cleaned_national_schbully_data
+cleaned_national_schbully_data$ hits  |> class() == "integer"
+cleaned_national_schbully_data$ month |> min(na.rm=TRUE) >= 0
+cleaned_national_schbully_data$ day|> max(na.rm=TRUE) <= 31
+
+
+#sum_bully_data
+sum_bully_data$ X  |> class() == "integer"
+sum_bully_data$ date  |> class() == "character"
+sum_bully_data$ sum  |> class() == "integer"
+
+
+#sum_cybully_data
+sum_cybully_data$ X  |> class() == "integer"
+sum_cybully_data$ date  |> class() == "character"
+sum_cybully_data$ sum  |> class() == "integer"
+
+
+#sum_schbully_data
+sum_schbully_data$ X  |> class() == "integer"
+sum_schbully_data$ date  |> class() == "character"
+sum_schbully_data$ sum  |> class() == "integer"
+
+
+#sum_national_schbully_data
+sum_national_schbully_data$ X  |> class() == "integer"
+sum_national_schbully_data$ date  |> class() == "character"
+sum_national_schbully_data$ sum  |> class() == "integer"
